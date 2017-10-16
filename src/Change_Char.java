263d0b2f1e816252b090963217585978aea41b2b
I432 <s10280202@gmail.com>
2017-10-16 15:00:19 +0800

    1016-2

---
+++ src/Change_Char.java
@@ -0,0 +1,12 @@
+public class Change_Char {
+
+    public static void main(String[] args) {
+
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        char ch = sc.next().charAt(0);
+        System.out.println((int)ch);
+        System.out.println(Integer.toHexString(20126));
+
+    }
+}
